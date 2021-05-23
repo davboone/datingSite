bce2329bd3c326b459d2727be69027642efc744e
David Boone <Dboone2@mail.greenriver.edu>
2021-05-22 23:35:58 -0400

    Completed 3 validation methods and added some styles

--- model/validation.php
+++ model/validation.php
@@ -3,18 +3,21 @@
 //validation.php
 //validate data sent to views directory
 
-function validName()
+function validName($name)
 {
 
+    return preg_match("/^[a-zA-Z]+$/",$name);
 }
 
-function validAge()
+function validAge($age)
 {
-
+    return (18 < $age) && ($age < 118);
 }
-function validPhone()
+function validPhone($phoneNum)
 {
+    $filterNum = str_replace("-","",$phoneNum);
 
+    return strlen($filterNum) == 10;
 }
 function validEmail()
 {
